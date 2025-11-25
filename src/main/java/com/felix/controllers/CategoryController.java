b0b891a9cc4fb216d457c5fcd9edc3c2cbc70bee
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-11-25 15:05:07 +0000

    feat: insert Category

--- src/main/java/com/felix/controllers/CategoryController.java
+++ src/main/java/com/felix/controllers/CategoryController.java
@@ -5,11 +5,10 @@ import com.felix.models.Category;
 import com.felix.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,5 +32,12 @@ public class CategoryController {
     public ResponseEntity<List<CategoryDTO>> findById(@PathVariable Long id){
         CategoryDTO dto = categoryService.findById(id);
         return ResponseEntity.ok().body(Collections.singletonList(dto));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = categoryService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.ok().body(dto);
     }
 }
