7e9ff6dec066c4c4870ed1e8f26e567f639e7f86
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-11-25 12:43:27 +0000

    feat: database seeding

--- src/main/java/com/felix/services/CategoryService.java
+++ src/main/java/com/felix/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.felix.services;
 
 import com.felix.models.Category;
 import com.felix.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional
     public List<Category> findAll(){
         return categoryRepository.findAll();
     }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,37 @@
+-- Inserindo categorias de loja na tabela 'categories'
+
+-- Categorias de produtos eletrônicos
+INSERT INTO categories (name) VALUES ('Eletrônicos');
+INSERT INTO categories (name) VALUES ('Computadores');
+INSERT INTO categories (name) VALUES ('Celulares');
+INSERT INTO categories (name) VALUES ('Acessórios Eletrônicos');
+
+-- Categorias de roupas e vestuário
+INSERT INTO categories (name) VALUES ('Roupas');
+INSERT INTO categories (name) VALUES ('Calçados');
+INSERT INTO categories (name) VALUES ('Acessórios de Moda');
+INSERT INTO categories (name) VALUES ('Bolsas e Mochilas');
+
+-- Categorias de alimentos e bebidas
+INSERT INTO categories (name) VALUES ('Alimentos');
+INSERT INTO categories (name) VALUES ('Bebidas');
+INSERT INTO categories (name) VALUES ('Doces e Sobremesas');
+INSERT INTO categories (name) VALUES ('Produtos Naturais');
+
+-- Categorias de casa e decoração
+INSERT INTO categories (name) VALUES ('Móveis');
+INSERT INTO categories (name) VALUES ('Decoração');
+INSERT INTO categories (name) VALUES ('Utensílios Domésticos');
+INSERT INTO categories (name) VALUES ('Iluminação');
+
+-- Categorias de saúde e bem-estar
+INSERT INTO categories (name) VALUES ('Saúde');
+INSERT INTO categories (name) VALUES ('Beleza');
+INSERT INTO categories (name) VALUES ('Suplementos');
+INSERT INTO categories (name) VALUES ('Fitness');
+
+-- Categorias de brinquedos e jogos
+INSERT INTO categories (name) VALUES ('Brinquedos');
+INSERT INTO categories (name) VALUES ('Jogos de Tabuleiro');
+INSERT INTO categories (name) VALUES ('Brinquedos Educativos');
+INSERT INTO categories (name) VALUES ('Jogos Eletrônicos');
